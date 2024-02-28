2bd899868b74c89a4ab7452f37171e16780b7310
MeSailor <themicroline@gmail.com>
2024-02-28 16:54:38 +0400

    add users table to database

--- src/database.js
+++ src/database.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const config = require("config");
 
 const { database, username, password, host, port } = config.get("dbConfig");
@@ -9,10 +9,39 @@ const sequelize = new Sequelize(database, username, password, {
   dialect: "mysql",
 });
 
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
 async function connect() {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
+    await sequelize.sync();
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
